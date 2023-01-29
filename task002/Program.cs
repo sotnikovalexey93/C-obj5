d721aad12970c3fb0408f57f832f1c1de50124a2
Alexey <sotnikovalexey93@mail.ru>
2023-01-29 10:35:28 +0300

    task002

--- task002/Program.cs
+++ task002/Program.cs
@@ -4,3 +4,35 @@
 
 [-4, -6, 89, 6] -> 0
 */
+
+
+Console.WriteLine("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(99,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+ 
+}
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
